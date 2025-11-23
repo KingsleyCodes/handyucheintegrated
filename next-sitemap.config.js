09d6ef1b08c5431ec3c4a3e7f55d57a98c9fd98b
KingsleyCodes <57971493+KingsleyCodes@users.noreply.github.com>
2025-11-23 18:59:30 +0100

    Go
    now

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteUrl: 'https://handyuche.com',
+  generateRobotsTxt: false, // we create robots.txt ourselves
+  changefreq: 'monthly',
+  priority: 0.7,
+  sitemapSize: 5000,
+}
